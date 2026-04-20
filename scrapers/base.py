0dbe577d32911de0b611c82122f864292821fb53
elyakoubim <mohamed.elyakoubi@gmail.com>
2026-04-20 11:42:42 +0200

    Restore to_airtable() method on Annonce (fixes 0 pushees)

--- scrapers/base.py
+++ scrapers/base.py
@@ -26,7 +26,18 @@ class Annonce:
     @property
     def id_unique(self) -> str:
         return hashlib.sha256(self.url.encode()).hexdigest()[:16]
-
+    def to_airtable(self) -> dict:
+                return {
+                                "url_annonce": self.url,
+                                "titre": self.titre[:255],
+                                "source_nom": self.source_nom,
+                                "pays": self.pays,
+                                "description": (self.type_vente + " " + self.description).strip()[:500],
+                                "date_scrappe": self.date_scrappe,
+                                "statut": "Nouveau",
+                                "vue": False,
+                }
+        
 
 class BaseScraper:
     def __init__(self, config: dict):
